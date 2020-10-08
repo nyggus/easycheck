a235dc7428bda336ae91bcd554d899d84384b4a4
Nyggus <nyggus@gmail.com>
2020-10-09 01:41:57 +0200

    __init__ updated so that import checkit works

---
+++ src/checkit/__init__.py
@@ -0,0 +1,21 @@
+from checkit.comparisons import (equal,
+                                 less_than, lt,
+                                 less_than_or_equal, lte,
+                                 greater_than, gt,
+                                 greater_than_or_equal, gte,
+                                 get_possible_operators,
+                                 )
+
+from checkit.checks import (check_if,
+                            check_if_not,
+                            check_instance,
+                            check_if_paths_exist,
+                            check_length,
+                            check_all_ifs,
+                            check_argument,
+                            check_comparison,
+                            ComparisonError,
+                            ArgumentValueError,
+                            LengthError,
+                            OperatorError,
+                            )
