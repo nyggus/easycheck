57e0ffd726cbf1e6171507e77da79c2376fbca26
nyggus <nyggus@gmail.com>
2021-01-22 06:26:32 +0100

    updated version and year

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="checkit",
-    version="0.0.1",
+    version="0.1.0",
     author="Nyggus & Ke Boan",
     author_email="nyggus@gmail.com",
     description="A tool for simple functionalized assertions in Python",
