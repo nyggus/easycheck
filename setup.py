7cc3458d0a6c3d0bbedd6b544d9e2fabe0b0ca4a
Ke Boan <4389744+keboan@users.noreply.github.com>
2021-01-26 19:11:54 +0100

    Add Python versions to metadata

--- setup.py
+++ setup.py
@@ -17,6 +17,10 @@ setuptools.setup(
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
