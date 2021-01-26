7d6f148c9491d506afd98fe8bfbd20c6ee09a6a7
Ke Boan <4389744+keboan@users.noreply.github.com>
2021-01-26 19:06:16 +0100

    Add more metadata

--- setup.py
+++ setup.py
@@ -8,15 +8,18 @@ setuptools.setup(
     version="0.1.0",
     author="Nyggus & Ke Boan",
     author_email="nyggus@gmail.com",
+    license='MIT',
     description="A tool for simple functionalized assertions in Python",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     url="https://github.com/nyggus/checkit",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires='>=3.6',
 )
