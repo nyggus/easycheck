9cc42becab6daba12b2f551bcae5299f68b5ca21
Marcin Kozak <39629775+nyggus@users.noreply.github.com>
2022-08-08 13:42:52 +0200

    Change version to v0.5.0
    (from v0.5.1)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ extras_requirements = {
 
 setuptools.setup(
     name="easycheck",
-    version="0.5.1",
+    version="0.5.0",
     author="Nyggus & Ke Boan",
     author_email="nyggus@gmail.com",
     license="MIT",
