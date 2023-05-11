4eb5bac1a2ff90a256468355c560280f73fa0250
nyggus <marcin.kozak@contractors.roche.com>
2023-05-11 09:32:08 +0200

    Increment version (v0.8.0) and add coauthor (Darsoo)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ extras_requirements = {
 setuptools.setup(
     name="easycheck",
     version="0.7.1",
-    author="Nyggus & Ke Boan",
+    author="Nyggus, Ke Boan & Darsoo",
     author_email="nyggus@gmail.com",
     license="MIT",
     description="A tool for checking conditions in Python",
