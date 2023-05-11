9fcf482104fa817cad1859d426b71cd0fb2e8568
nyggus <marcin.kozak@contractors.roche.com>
2023-05-11 09:35:56 +0200

    update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ extras_requirements = {
 
 setuptools.setup(
     name="easycheck",
-    version="0.7.1",
+    version="0.8.0",
     author="Nyggus, Ke Boan & Darsoo",
     author_email="nyggus@gmail.com",
     license="MIT",
